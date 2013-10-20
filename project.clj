e4b23cf06245fe95b74ea5b99f8d400ff0dcd7b6
James Henderson <james@pcr.me.uk>
2013-10-20 18:31:04 +0100

    Run `lein new splat clojurex-demo`

---
+++ project.clj
@@ -0,0 +1,38 @@
+(defproject tetris ""
+
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  
+  :dependencies [[org.clojure/clojure "1.5.1"]
+
+                 [ring/ring-core "1.2.0"]
+                 [compojure "1.1.5"]
+                 [hiccup "1.0.4"]
+
+                 [prismatic/dommy "0.1.1"]
+
+                 [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]
+                 [org.clojure/clojurescript "0.0-2080"]
+                 [org.clojure/tools.reader "0.8.0"]
+
+                 [jarohen/chord "0.2.1"]]
+
+  :plugins [[jarohen/lein-frodo "0.2.3"]
+            [lein-cljsbuild "1.0.0"]
+            [lein-pdo "0.1.1"]]
+
+  :frodo/config-resource "tetris-config.edn"
+
+  :source-paths ["src/clojure"]
+
+  :resource-paths ["resources" "target/resources"]
+
+  :cljsbuild {:builds [{:source-paths ["src/cljs"]
+                        :compiler {:output-to "target/resources/js/tetris.js"
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]}
+
+  :aliases {"dev" ["pdo" "cljsbuild" "auto," "frodo"]
+            "start" ["do" "cljsbuild" "once," "trampoline" "frodo"]})
---
+++ src/cljs/tetris/cljs/app.cljs
@@ -0,0 +1,26 @@
+(ns tetris.cljs.app
+    (:require [clojure.string :as s]
+              [tetris.cljs.home :as home]
+              clojure.browser.repl)
+    (:require-macros [dommy.macros :refer [sel sel1]]))
+
+(def default-hash "#/")
+
+(defn- bind-hash [!hash]
+  (letfn [(on-hash-change []
+            (reset! !hash (.-hash js/location)))]
+    
+    (set! (.-onhashchange js/window) on-hash-change)
+    
+    (when (s/blank? (.-hash js/location))
+      (set! (.-hash js/location) default-hash))
+    
+    (on-hash-change)))
+
+(set! (.-onload js/window)
+      (fn []
+        (let [!hash (atom nil)]
+          (home/watch-hash! !hash)
+          (bind-hash !hash))))
+
+
