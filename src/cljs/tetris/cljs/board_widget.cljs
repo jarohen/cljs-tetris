544f023df0338daaa09ca23d6b2befe805e996b6
James Henderson <james@pcr.me.uk>
2013-10-31 22:34:44 +0000

    Rendering shapes when state atom changed in REPL

--- src/cljs/tetris/cljs/board_widget.cljs
+++ src/cljs/tetris/cljs/board_widget.cljs
@@ -2,17 +2,25 @@
   (:require [dommy.core :as d]
             [tetris.cljs.game-board :as gb]
             [tetris.cljs.canvas :refer [make-canvas]]
-            [tetris.cljs.tetraminos :as t])
+            [tetris.cljs.tetraminos :as t]
+
+            [tetris.cljs.board :as b])
   (:require-macros [dommy.macros :refer [node sel1]]))
 
 (defn render-tetramino! [game-board {:keys [shape location rotation color] :as piece}]
   (gb/color-cells! game-board (t/piece->cells piece) color))
 
+(defn render-current-piece! [game-board {old-piece :current-piece} {new-piece :current-piece}]
+  (when (not= old-piece new-piece)
+    (when old-piece
+      (render-tetramino! game-board (assoc old-piece :color "white")))
+    (when new-piece
+      (render-tetramino! game-board new-piece))))
+
 (defn watch-game! [game-board !game]
   (add-watch !game ::renderer
-             (fn [_ _ old-game new-game]
-               (js/console.log (pr-str {:old old-game
-                                        :new new-game})))))
+            (fn [_ _ old-game new-game]
+              (render-current-piece! game-board old-game new-game))))
 
 (defn make-board-widget [!game]
   (def !test-game !game)
@@ -20,3 +28,11 @@
   (let [game-board (doto (make-canvas)
                      (watch-game! !game))]
     (gb/board->node game-board)))
+
+(comment
+  (reset! !test-game
+          (let [{:keys [blocks-wide blocks-tall]} b/canvas-size]
+            {:current-piece {:shape (rand-nth (vec t/shapes))
+                             :color (rand-nth (vec t/colors))
+                             :rotation (rand-int 4)
+                             :location (map rand-int [blocks-wide blocks-tall])}})))
