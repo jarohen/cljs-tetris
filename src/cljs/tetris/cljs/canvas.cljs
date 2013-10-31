a58ceec1ec66b13285d677a5b62d862a3ced75c7
James Henderson <james@pcr.me.uk>
2013-10-31 08:04:40 +0000

    Creating 'render-grid!'

--- src/cljs/tetris/cljs/canvas.cljs
+++ src/cljs/tetris/cljs/canvas.cljs
@@ -2,8 +2,15 @@
   (:require [tetris.cljs.board :as b])
   (:require-macros [dommy.macros :refer [node]]))
 
-(defn make-canvas []
+(defn canvas-node []
   (node
    (let [{:keys [blocks-tall blocks-wide]} b/canvas-size]
      [:canvas {:height (* b/block-size blocks-tall)
                :width (* b/block-size blocks-wide)}])))
+
+(defn render-grid! [$canvas]
+  (def $test-canvas $canvas))
+
+(defn make-canvas []
+  (doto (canvas-node)
+    (render-grid!)))
