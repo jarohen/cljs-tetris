fc49a2cf0a024d68e72827cf25194bde7cfe06be
James Henderson <james@pcr.me.uk>
2013-10-31 08:33:15 +0000

    Moving grid REPL code into 'render-grid!'

--- src/cljs/tetris/cljs/canvas.cljs
+++ src/cljs/tetris/cljs/canvas.cljs
@@ -9,20 +9,14 @@
                :width (* b/block-size blocks-wide)}])))
 
 (defn render-grid! [$canvas]
-  (def $test-canvas $canvas))
+  (let [context (.getContext $canvas "2d")
+        {:keys [blocks-tall blocks-wide]} b/canvas-size]
+    (doseq [i (range blocks-wide)
+            j (range blocks-tall)]
+      (.strokeRect context
+                   (* i b/block-size)
+                   (* j b/block-size) b/block-size b/block-size))))
 
 (defn make-canvas []
   (doto (canvas-node)
     (render-grid!)))
-
-(comment
-  (let [$canvas $test-canvas]
-    (let [context (.getContext $canvas "2d")
-          {:keys [blocks-tall blocks-wide]} b/canvas-size]
-      (doseq [i (range blocks-wide)
-              j (range blocks-tall)]
-        (.strokeRect context
-                     (* i b/block-size)
-                     (* j b/block-size)
-                     b/block-size
-                     b/block-size)))))
