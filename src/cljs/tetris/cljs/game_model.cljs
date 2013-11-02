e25cc8a0ac3383618ef5a95561a4a7d66dc5e1d1
James Henderson <james@pcr.me.uk>
2013-11-02 14:35:17 +0000

    Got a starting piece

--- src/cljs/tetris/cljs/game_model.cljs
+++ src/cljs/tetris/cljs/game_model.cljs
@@ -1,10 +1,30 @@
 (ns tetris.cljs.game-model
   (:require [tetris.cljs.board :as b]
+            [tetris.cljs.tetraminos :as t]
             [cljs.core.async :as a])
   (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
 
+(defn valid-starting-locations [{:keys [shape rotation] :as piece}]
+  (let [cells (t/piece->cells (assoc piece :location [0 0]))
+        {:keys [blocks-wide]} b/canvas-size
+        ;; one row visible
+        y (- (apply max (map second cells)))]
+
+    ;; all of shape has to fit in width of canvas
+    (for [valid-x (range (- (apply min (map first cells)))
+                         (- blocks-wide (apply max (map first cells))))]
+      [valid-x y])))
+
+(defn random-piece []
+  (let [shape (rand-nth (vec t/shapes))
+        rotation (rand-int 4)]
+    {:shape shape
+     :color (rand-nth (vec t/colors))
+     :rotation rotation
+     :location (rand-nth (valid-starting-locations {:shape shape :rotation rotation}))}))
+
 (defn new-game []
-  )
+  {:current-piece (random-piece)})
 
 (defn repeatedly-tick! [!game]
   )
