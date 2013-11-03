40fbe900a9c382e71f729380bee5261aef29f283
James Henderson <james@pcr.me.uk>
2013-11-03 21:59:07 +0000

    Making websockets work

--- src/cljs/tetris/cljs/multiplayer_model.cljs
+++ src/cljs/tetris/cljs/multiplayer_model.cljs
@@ -1,12 +1,33 @@
 (ns tetris.cljs.multiplayer-model
   (:require [cljs.core.async :as a]
-            [chord.client :refer [ws-ch]])
+            [chord.client :refer [ws-ch]]
+            [cljs.reader :refer [read-string]])
   (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
+
+(def ws-url
+  (let [loc js/location]
+    (str "ws://" (.-host loc) "/scores")))
+
+(defn send-scores! [name !game-model scores-ch]
+  (a/put! scores-ch {:player name :score (or (:cleared-row-count @!game-model) 0)})
+  (add-watch !game-model ::multiplayer
+             (fn [_ _ old-model new-model]
+               (let [{old-count :cleared-row-count} old-model
+                     {new-count :cleared-row-count} new-model]
+                 (when (not= old-count new-count)
+                   (a/put! scores-ch {:player name :score (or new-count 0)}))))))
+
+(defn watch-other-scores! [scores-ch !top-scores]
+  (go-loop []
+    (reset! !top-scores (read-string (:message (a/<! scores-ch))))
+    (recur)))
 
 (defn wire-up-multiplayer! [!game-model !top-scores !player-name commands-ch]
   (go
-   (let [{:keys [name]} (a/<! commands-ch)
-         scores-ch (a/<! (ws-ch "ws://localhost:3000/scores"))]
-     (a/>! scores-ch (pr-str {:name name}))
-     (js/console.log "Client received: '" (:message (a/<! scores-ch)) "'"))))
+    (let [{:keys [name]} (a/<! commands-ch)
+          scores-ch (a/<! (ws-ch ws-url))]
+      (send-scores! name !game-model scores-ch)
+      (when (= :success (read-string (:message (a/<! scores-ch))))
+        (watch-other-scores! scores-ch !top-scores)
+        (reset! !player-name name)))))
 
--- src/clojure/tetris/multiplayer.clj
+++ src/clojure/tetris/multiplayer.clj
@@ -3,9 +3,41 @@
             [clojure.core.async :refer [<! >! put! close! go go-loop]]
             [clojure.tools.reader.edn :as edn]))
 
+(def connected-users (agent {}))
+
+(defn send-scores! [conns]
+  (let [scores (->> conns
+                    vals
+                    (sort-by :score >)
+                    (take 10)
+                    (map #(dissoc % :ch))
+                    pr-str)]
+    (doseq [ch (keys conns)]
+      (put! ch scores))))
+
+(defn user-disconnected [conns {:keys [ch] :as conn}]
+  (doto (dissoc conns ch)
+    send-scores!))
+
+(defn update-score [conns {:keys [ch score] :as conn}]
+  (doto (assoc-in conns [ch :score] score)
+    send-scores!))
+
+(defn add-connection [conns {:keys [ch] :as conn}]
+  (let [new-conns (assoc conns ch conn)]
+    (send-scores! new-conns)
+    (go-loop []
+      (if-let [{:keys [message]} (<! ch)]
+        (let [{:keys [score]} (edn/read-string message)]
+          (send-off connected-users update-score (assoc conn :score score))
+          (recur))
+        (send-off connected-users user-disconnected conn)))
+    new-conns))
+
 (defn user-joined! [req]
   (with-channel req ws-ch
-    (go-loop []
-      (when-let [{:keys [message]} (<! ws-ch)]
-        (>! ws-ch (str "Hi client! Server received: " message))
-        (recur)))))
+    (go
+      (let [{:keys [message]} (<! ws-ch)
+            {:keys [player score] :as user} (edn/read-string message)]
+        (>! ws-ch (pr-str :success))
+        (send-off connected-users add-connection (assoc user :ch ws-ch))))))
